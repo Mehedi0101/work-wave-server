5fec8820d13a0377b06b98e416af35623058f8c5
Mehedi0101 <mehedi29121998@gmail.com>
2023-12-22 16:18:54 +0600

    deployed in vercel

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://work-wave-f93a2.web.app','https://console.firebase.google.com/project/work-wave-f93a2/overview'],
     credentials: true
 }));
 app.use(express.json());
@@ -96,8 +96,6 @@ async function run() {
             const result = await taskCollection.deleteOne(query);
             res.send(result);
         })
-
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
     }
